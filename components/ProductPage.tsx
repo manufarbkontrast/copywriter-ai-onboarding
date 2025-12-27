ac1feb96788eba72fc5a4f654654b8a6f8bd1dc0
manufarbkontrast <manuwolram1@gmail.com>
2025-12-27 02:23:11 +0100

    Fix opacity syntax for background glows

--- components/ProductPage.tsx
+++ components/ProductPage.tsx
@@ -219,14 +219,16 @@ const ProductPage: React.FC<Props> = ({ onStartOnboarding }) => {
         {/* Animated Background Glows - sehr subtil */}
         <div className="absolute inset-0 overflow-hidden pointer-events-none">
           <div 
-            className="absolute top-1/4 left-1/4 w-96 h-96 bg-white rounded-full blur-3xl opacity-2"
+            className="absolute top-1/4 left-1/4 w-96 h-96 bg-white rounded-full blur-3xl"
             style={{
+              opacity: 0.02,
               transform: `translate(${scrollY * 0.2}px, ${scrollY * 0.3}px) scale(${1 + scrollY * 0.0002})`
             }}
           />
           <div 
-            className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-white rounded-full blur-3xl opacity-2"
+            className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-white rounded-full blur-3xl"
             style={{
+              opacity: 0.02,
               transform: `translate(${-scrollY * 0.2}px, ${-scrollY * 0.3}px) scale(${1 + scrollY * 0.0002})`
             }}
           />
