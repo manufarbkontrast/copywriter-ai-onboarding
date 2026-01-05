5bb2941ccd3ec984f30e602fc2cfd1f69f24d4b7
manufarbkontrast <manuwolram1@gmail.com>
2026-01-05 08:43:31 +0100

    TargetCursor Rahmen zu schwarz geändert

--- components/TargetCursor.tsx
+++ components/TargetCursor.tsx
@@ -34,7 +34,7 @@ const TargetCursor: React.FC = () => {
         transform: 'translate(-50%, -50%)',
       }}
     >
-      <div className="w-8 h-8 rounded-full border-2 border-white transition-all duration-200 ease-out">
+      <div className="w-8 h-8 rounded-full border-2 border-black transition-all duration-200 ease-out">
         <div className="w-2 h-2 rounded-full bg-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
       </div>
     </div>
